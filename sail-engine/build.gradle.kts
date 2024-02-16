3fe1239983200d9bf128f44c28e96dcb5d464463
davidhorby <dhorby@gmail.com>
2024-02-16 09:41:44 +0000

    Update sail engine gradle build to kts

---
+++ sail-engine/build.gradle.kts
@@ -0,0 +1,54 @@
+val invoker: Configuration by configurations.creating
+
+//https://github.com/mwhyte-dev/kotlin-google-cloud-function/blob/main/build.gradle.kts
+
+plugins {
+    alias(libs.plugins.shadowJar)
+}
+
+configurations {
+    invoker
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.dhorby.gcloud.BucketToDSFunc"
+    }
+}
+
+dependencies {
+
+    implementation(project(":domain"))
+    implementation(project(":shared"))
+
+    implementation(libs.bundles.kotlin)
+    implementation(libs.bundles.googleCloud)
+    implementation(libs.bundles.jackson)
+
+    invoker("com.google.cloud.functions.invoker:java-function-invoker:1.1.1")
+
+    testImplementation(testFixtures(project(":shared")))
+//    testImplementation(project(mapOf(":domain" to "testArtifact")))
+
+    testImplementation(libs.bundles.test)
+    testRuntimeOnly(libs.bundles.testRuntime)
+
+}
+
+task<JavaExec>("runFunction") {
+    mainClass = "com.google.cloud.functions.invoker.runner.Invoker"
+    classpath(invoker)
+    inputs.files(configurations.runtimeClasspath, sourceSets["main"].output)
+    args(
+        "--target", project.findProperty("runFunction.target") ?: "",
+        "--port", project.findProperty("runFunction.port") ?: 8080
+    )
+    doFirst {
+        args("--classpath", files(configurations.runtimeClasspath, sourceSets["main"].output).asPath)
+    }
+}
+
+
+
+
+
