6d390b7b627d3429ec9e6a8fc80d1ac40bc05f17
davidhorby <david.horby@springernature.com>
2024-12-06 10:54:12 +0000

    Add WaveAdapterTest

--- wave-app/src/test/kotlin/com/dhorby/wavemapper/adapter/WaveAdapterTest.kt
+++ wave-app/src/test/kotlin/com/dhorby/wavemapper/adapter/WaveAdapterTest.kt
@@ -1,21 +1,38 @@
 package com.dhorby.wavemapper.adapter
 
 import AddRequestCount
+import com.dhorby.gcloud.external.junit.DataStoreExtension
 import com.dhorby.gcloud.external.storage.DataStoreClient
 import com.dhorby.gcloud.model.GeoLocation
 import com.dhorby.gcloud.model.PieceLocation
 import com.dhorby.gcloud.model.PieceType
-import com.dhorby.wavemapper.fake.FakeDatastore
+import com.dhorby.gcloud.wavemapper.WaveServiceFunctions
+import com.dhorby.wavemapper.external.google.GoogleMapsClient
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.present
+import org.http4k.client.ApacheClient
 import org.http4k.events.AutoMarshallingEvents
 import org.http4k.events.AutoMarshallingEvents.invoke
 import org.http4k.events.Event
 import org.http4k.events.EventFilters
 import org.http4k.events.then
 import org.http4k.format.Jackson
+import org.http4k.template.ViewModel
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 
 class WaveAdapterTest {
 
-    private val datastore = FakeDatastore().dataStore
+    companion object {
+        @JvmStatic
+        @RegisterExtension
+        val server = DataStoreExtension()
+            .builder()
+            .build()
+    }
+
+    private val datastore = server.localDatastoreHelper.options.service
+
     private val events: (Event) -> Unit =
         EventFilters.AddTimestamp()
             .then(EventFilters.AddEventName())
@@ -31,16 +48,16 @@ class WaveAdapterTest {
         pieceType = PieceType.BOAT
     )
 
-//    @Test
-//    fun `wave page should be present`(){
-//        val waveAdapter = WaveAdapter(
-//            siteListFunction = WaveServiceFunctions.siteListFunction,
-//            dataForSiteFunction = WaveServiceFunctions.dataForSiteFunction,
-//            storageAdapter = storageAdapter,
-//            googleMapsClient = GoogleMapsClient(ApacheClient())
-//        )
-//        storageAdapter.add(pieceLocation)
-////        val wavePage: ViewModel? = waveAdapter.getWavePage()
-////        assertThat(wavePage, present())
-//    }
+    @Test
+    fun `wave page should be present`(){
+        val waveAdapter = WaveAdapter(
+            siteListFunction = WaveServiceFunctions.siteListFunction,
+            dataForSiteFunction = WaveServiceFunctions.dataForSiteFunction,
+            storageAdapter = storageAdapter,
+            googleMapsClient = GoogleMapsClient(ApacheClient())
+        )
+        storageAdapter.add(pieceLocation)
+        val wavePage: ViewModel? = waveAdapter.getWavePage()
+        assertThat(wavePage, present())
+    }
 }
